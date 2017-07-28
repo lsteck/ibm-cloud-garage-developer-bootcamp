0ac09f313d134426fa7c4f5c352ce4426819503f
Larry Steck <lsteck@us.ibm.com>
2017-07-28 15:19:28 -0500

    add isPalndrome logic

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,8 +3,8 @@ describe.only('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
-  function isPalindrome(){
-    return true;
+  function isPalindrome(word){
+    return word.split('').reverse().join('') === word;
   }
 
   describe('a palindrome', () => {
