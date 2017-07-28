f2be2a1e2156e32a00b034210fc55e95813d2c04
Larry Steck <lsteck@us.ibm.com>
2017-07-28 15:46:41 -0500

    add isPalndrome logic complete

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,18 +3,36 @@ describe.only('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
-  function isPalindrome(word){
-    return word.split('').reverse().join('') === word;
+  function isPalindrome(phrase){
+    if (phrase.trim().length === 0){
+      return false;
+    } else {
+      return phrase === phrase.split('').reverse().join('');
+    }
   }
 
   describe('a palindrome', () => {
     it('is racecar', () => {
       isPalindrome('racecar').should.be.true();
     });
-    it('is not larry');
-    it('is not race car');
-    it('is not "   "');
-    it('is not ""');
-    it('is "mom dad mom"');
+
+    it('is not larry', () => {
+      isPalindrome('larry').should.be.false();
+    });
+
+    it('is not race car', () => {
+      isPalindrome('race car').should.be.false();
+    });
+
+    it('is not "   "', () => {
+      isPalindrome('     ').should.be.false();
+        });
+    it('is not ""', () => {
+      isPalindrome('').should.be.false();
+    });
+
+    it('is "mom dad mom"', () => {
+      isPalindrome('mom dad mom').should.be.true();
+    });
   });
 });
