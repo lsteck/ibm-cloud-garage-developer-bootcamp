cadf027df6a25fc2896eeffb33fb1f8646770668
Larry Steck <lsteck@us.ibm.com>
2017-07-28 14:42:30 -0500

    first palindrome

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,2 +1,20 @@
-describe('the palindrome canary spec', () => {
+describe.only('the palindrome canary spec', () => {
+  it('shows the infrastructure works', () => {
+    true.should.be.true();
+  });
+
+  function isPalindrome(){
+    return true;
+  }
+
+  describe('a palindrome', () => {
+    it('is racecar', () => {
+      isPalindrome('racecar').should.be.true();
+    });
+    it('is not larry');
+    it('is not race car');
+    it('is not "   "');
+    it('is not ""');
+    it('is "mom dad mom"');
+  });
 });
