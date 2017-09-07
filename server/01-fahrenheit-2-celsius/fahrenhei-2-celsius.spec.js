3578d149105ffcc8004fbf66ab0177278b389cf2
Larry Steck <lsteck@us.ibm.com>
2017-09-07 17:34:57 -0500

    complete fahrenheit2celsius

--- server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenhei-2-celsius.spec.js
@@ -3,12 +3,15 @@ describe.only('the fahrenheit-2-celsius canary spec', () => {
     true.should.be.true();
   });
 
-  let fahrenheit2celsius = () => 0;
+  let fahrenheit2celsius = (fahrenheit) => (fahrenheit - 32) * 5 / 9;
 
   describe('fahrenheit-2-celsius behaves in the following way', () => {
     it('fahrenheit2celsius(32) equals 0', () => {
       fahrenheit2celsius(32).should.equal(0);
     });
-    it('fahrenheit2celsius(50) equals 10');
+
+    it('fahrenheit2celsius(50) equals 10', () => {
+      fahrenheit2celsius(50).should.equal(10);
+    });
   });
 });
