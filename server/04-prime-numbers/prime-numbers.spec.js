cf5aa73270c14f254ce6c590ea86fac62124bad0
Larry Steck <lsteck@us.ibm.com>
2017-09-18 14:59:32 -0400

    PrimeNumbers test

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -5,20 +5,52 @@ describe.only('the prime numbers canary spec', () => {
     true.should.be.true();
   });
 
-  let primeFactors = () => [];
+  let primeFactors = (number) => {
+    let factors = [];
+    if (number > 1){
+      while (number % 2 === 0){
+        factors.push(2);
+        number /= 2;
+      }
+    }
+    if (number > 1) factors.push(number);
+    return factors;
+  };
 
   describe('a prime numbers function should', () => {
     it('return none for 1', () => {
       primeFactors(1).should.deepEqual([]);
     });
 
-    it('return 2 for 2');
-    it('return 3 for 3');
-    it('return 2, 2 for 4');
-    it('return 5 for 5');
-    it('return 2, 3 for 6');
-    it('return 7 for 7');
-    it('return 2, 2, 2 for 8');
+    it('return 2 for 2', () => {
+      primeFactors(2).should.deepEqual([2]);
+    });
+
+    it('return 3 for 3', () => {
+      primeFactors(3).should.deepEqual([3]);
+    });
+
+    it('return 2, 2 for 4', () => {
+      primeFactors(4).should.deepEqual([2, 2]);
+    });
+
+    it('return 5 for 5', () => {
+      primeFactors(5).should.deepEqual([5]);
+    });
+
+
+    it('return 2, 3 for 6', () => {
+      primeFactors(6).should.deepEqual([2, 3]);
+    });
+
+    it('return 7 for 7', () => {
+      primeFactors(7).should.deepEqual([7]);
+    });
+
+    it('return 2, 2, 2 for 8', () => {
+      primeFactors(8).should.deepEqual([2, 2, 2]);
+    });
+
     it('return 3, 3 for 9');
   });
 });
